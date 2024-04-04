6d770d4f29557a4163acc50e8e40d36d91ba5e7a
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2024-04-04 21:12:03 +0300

    configure database connection with env support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'process';
+
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      username: 'root',
+      password: process.env.DATABASE_PASSWORD,
+      database: 'film_inventory',
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
