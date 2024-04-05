793dbbfcff8da28524b562f95340e6e107625017
Nikita Denysenko <53399334+NikitaDenysenko@users.noreply.github.com>
2024-04-05 11:12:15 +0300

    add env to typeorm forRoot config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as process from 'process';
-
 import { ConfigModule } from '@nestjs/config';
+import { Inventory } from '../typeorm/entities/Inventory';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       username: 'root',
+      port: 3306,
       password: process.env.DATABASE_PASSWORD,
       database: 'film_inventory',
+      entities: [Inventory],
       synchronize: true,
-      entities: [],
     }),
   ],
   controllers: [AppController],
